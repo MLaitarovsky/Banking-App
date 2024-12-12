cc0fcbd2d2c456f6e130fe7517f818cc353b0ace
Michael <laytarovsky.michael@gmail.com>
2024-12-12 01:07:15 -0500

    Adding components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,29 @@
-import React from "react"
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import HeaderBox from "@/components/ui/HeaderBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = { firstName: "Michael" };
+
   return (
-    <div>
-      Home
-    </div>
-  )
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
